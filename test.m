e85a5043a2152e58e753618c3f34cc481e8d1b54
SamSSY <sam7822567@gmail.com>
2016-03-22 15:42:25 +0800

    0322/1542

--- test.m
+++ test.m
@@ -6,8 +6,8 @@ OriIm = imread('../screenshot/exp1_1_2lighter_025_pen.bmp');
 ColorIm=OriIm;
 GrayIm= rgb2gray(OriIm);
 
-choppedColorIm = ColorIm([1: 375], [250: 400], [1: 3]);
-choppedGrayIm=GrayIm([1: 375], [250: 400]);
+choppedColorIm = ColorIm([90: 140], [260: 320], [1: 3]);
+choppedGrayIm=GrayIm([90: 140], [260: 320]);
 
 
 figure
@@ -50,39 +50,58 @@ figure
 plot(VerticaldbIm)
 title('Vertical projection')
 
+% interpolation
+x = 1:length(VerticaldbIm);
+xx = 1:0.01:length(VerticaldbIm);
+InterpolatedVerticaldbIm = spline(x,VerticaldbIm,xx);
 
-idx = find(VerticaldbIm >= -6 );
+% idx = find(VerticaldbIm >= -6 );
+idx = find(InterpolatedVerticaldbIm >= -6 );
 leftBound = idx(1);
+index = 1;
+Width6dBArr = [];
 disp('vertical Width6dB:');
 for i = 1:length(idx)
     if(i ~= length(idx))
         temp = idx(i + 1) - idx(i);
     end
+    Width6dBArr(index) = idx(i) - leftBound;
     if(temp ~= 1)
-        Width6dB = idx(i) - leftBound;
-        disp(Width6dB);
+        index = index + 1;
         leftBound = idx(i + 1);
     end
 end
 
-idx2 = find(LateraldbIm >= -6 );
-index = 1;
-Width6dB = 1;
+for i = 1: length(Width6dBArr)
+    disp(Width6dBArr(i));
+end
+
+% interpolation
+x = 1:length(LateraldbIm);
+xx = 1:0.01:length(LateraldbIm);
+InterpolatedLateraldbIm = spline(x,LateraldbIm,xx);
+
+idx2 = find(InterpolatedLateraldbIm >= -6 );
 leftBound = idx2(1);
+index = 1;
+Width6dBArr2 = [];
 disp('lateral Width6dB:');
 for i = 1:length(idx2)
     if(i ~= length(idx2))
         temp = idx2(i + 1) - idx2(i);
     end
+    Width6dBArr2(index) = idx2(i) - leftBound;
     if(temp ~= 1)
-        Width6dB = idx2(i) - leftBound;
-        disp(Width6dB);
+        index = index + 1;
         leftBound = idx2(i + 1);
     end
 end
 
+for i = 1: length(Width6dBArr2)
+    disp(Width6dBArr2(i));
+end
 %%%%%%%Speckle
-
+%{
 [xdim, ydim] = size(dbImDouble);
 
 EBase = zeros(xdim, ydim);
@@ -100,13 +119,13 @@ histogramI=max(IBase);
 
 figure
 % 請先把 LinearIm_I 以及 LinearIm_E 變成column vector, 再使用hist指令畫出機率分布圖
-hist(histogramE,500);	% 像expenential distribution嗎?
+hist(histogramE,500);   % 像expenential distribution嗎?
 title('Speckle Intensity Distribution');xlabel('I');ylabel('P_I')
 
 figure
-hist(histogramI,1000);	% 像Reyleigh distribution嗎?
+hist(histogramI,1000);  % 像Reyleigh distribution嗎?
 title('Speckle Amplitude Distribution');xlabel('E');ylabel('P_E')
-
+%}
 
 
 
